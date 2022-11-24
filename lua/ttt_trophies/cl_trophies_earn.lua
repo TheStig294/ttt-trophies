2efc1616048eda2ed122119d882d987d2d887fe1
The Stig <TheStig294@users.noreply.github.com>
2022-11-24 14:48:40 +1030

    Fixed lua error trying to read the saved players name as a trophy in earned trophies stats file

--- lua/ttt_trophies/cl_trophies_earn.lua
+++ lua/ttt_trophies/cl_trophies_earn.lua
@@ -10,6 +10,8 @@ net.Receive("TTTSendEarnedTrophies", function()
         local trophyID = net.ReadString()
         -- Trophies would have loaded in by now, as the InitPostEntity hook delays when this net message is sent
         local trophy = TTTTrophies.trophies[trophyID]
+        -- Need to skip over the player's name saved in the earned trophies stats file, or anything else in the earned trophies file that isn't on the trophies list
+        if not trophy then return end
         trophy.earned = true
     end
 end)
