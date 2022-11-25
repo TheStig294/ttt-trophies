8b83b5b297244c07e31a4c8d60443980396f91cf
The Stig <TheStig294@users.noreply.github.com>
2022-11-25 15:35:06 +1030

    Added trigger for karma round trophy

--- lua/ttt_trophies/trophies/karmaRound.lua
+++ lua/ttt_trophies/trophies/karmaRound.lua
@@ -3,4 +3,29 @@ TROPHY.id = "karmaRound"
 TROPHY.title = "Rythian-Doing-Murders"
 TROPHY.desc = "Don't lose karma for a whole round"
 TROPHY.rarity = 1
+
+function TROPHY:Trigger()
+    local noKarmaLostPlayers = {}
+
+    self:AddHook("TTTBeginRound", function()
+        for _, ply in ipairs(player.GetAll()) do
+            if self:PlayerAlive(ply) then
+                table.insert(noKarmaLostPlayers, ply)
+            end
+        end
+    end)
+
+    self:AddHook("TTTKarmaGivePenalty", function(ply, penalty, victim)
+        table.RemoveByValue(noKarmaLostPlayers, ply)
+    end)
+
+    self:AddHook("TTTEndRound", function()
+        if not table.IsEmpty(noKarmaLostPlayers) and noKarmaLostPlayers ~= {} then
+            self:Earn(noKarmaLostPlayers)
+        end
+
+        table.Empty(noKarmaLostPlayers)
+    end)
+end
+
 RegisterTTTTrophy(TROPHY)
