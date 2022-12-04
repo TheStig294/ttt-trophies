a47ddf576552803ce0178a1077889820b0e72167
The Stig <TheStig294@users.noreply.github.com>
2022-12-04 16:42:14 +1030

    Added medium trophy

---
+++ lua/ttt_trophies/trophies/medium.lua
@@ -0,0 +1,28 @@
+local TROPHY = {}
+TROPHY.id = "medium"
+TROPHY.title = "Sometimes you can trust the ghosts..."
+TROPHY.desc = "As a Medium, kill a traitor while a dead player is near them"
+TROPHY.rarity = 3
+
+function TROPHY:Trigger()
+    self.roleMessage = ROLE_MEDIUM
+
+    self:AddHook("DoPlayerDeath", function(ply, attacker, dmg)
+        if not IsPlayer(attacker) or not attacker:IsMedium() or not TTTTrophies:IsTraitorTeam(ply) then return end
+        local entTbl = ents.FindInSphere(ply:GetPos(), 100)
+
+        for _, ent in ipairs(entTbl) do
+            if IsPlayer(ent) and not self:IsAlive(ent) and ent ~= ply then
+                self:Earn(attacker)
+
+                return
+            end
+        end
+    end)
+end
+
+function TROPHY:Condition()
+    return ConVarExists("ttt_medium_enabled") and GetConVar("ttt_medium_enabled"):GetBool()
+end
+
+RegisterTTTTrophy(TROPHY)
