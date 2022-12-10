24061c46ba9e732a0804f31649d2ab421ddbf8aa
The Stig <TheStig294@users.noreply.github.com>
2022-12-10 17:14:37 +1030

    Added turncoat trophy

---
+++ lua/ttt_trophies/trophies/turncoat.lua
@@ -0,0 +1,36 @@
+local TROPHY = {}
+TROPHY.id = "turncoat"
+TROPHY.title = "Twist ending"
+TROPHY.desc = "As a Turncoat, win the round within 2 seconds of turning"
+TROPHY.rarity = 2
+
+function TROPHY:Trigger()
+    self.roleMessage = ROLE_TURNCOAT
+    local turnedPlayers = {}
+
+    self:AddHook("TTTTurncoatTeamChanged", function(ply, traitor)
+        if traitor then
+            turnedPlayers[ply] = true
+
+            timer.Simple(2, function()
+                turnedPlayers[ply] = false
+            end)
+        end
+    end)
+
+    self:AddHook("TTTEndRound", function(result)
+        if result == WIN_TRAITOR then
+            for ply, value in pairs(turnedPlayers) do
+                if value == true then
+                    self:Earn(ply)
+                end
+            end
+        end
+    end)
+end
+
+function TROPHY:Condition()
+    return ConVarExists("ttt_turncoat_enabled") and GetConVar("ttt_turncoat_enabled"):GetBool()
+end
+
+RegisterTTTTrophy(TROPHY)
