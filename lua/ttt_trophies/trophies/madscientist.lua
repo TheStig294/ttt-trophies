f3e5b4593e82a5a711c0a6779bc231e4dc7c1037
The Stig <TheStig294@users.noreply.github.com>
2022-12-12 23:07:08 +1030

    Added mad scientist trophy

---
+++ lua/ttt_trophies/trophies/madscientist.lua
@@ -0,0 +1,21 @@
+local TROPHY = {}
+TROPHY.id = "madscientist"
+TROPHY.title = "Nope, doesn't work!"
+TROPHY.desc = "As a Mad Scientist, try to revive someone that's already a zombie"
+TROPHY.rarity = 2
+
+function TROPHY:Trigger()
+    self.roleMessage = ROLE_MADSCIENTIST
+
+    self:AddHook("TTTMadScientistZombifyBegin", function(ply, tgt)
+        if tgt:IsZombie() then
+            self:Earn(ply)
+        end
+    end)
+end
+
+function TROPHY:Condition()
+    return ConVarExists("ttt_madscientist_enabled") and GetConVar("ttt_madscientist_enabled"):GetBool()
+end
+
+RegisterTTTTrophy(TROPHY)
