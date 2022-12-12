009de8a2ca20a6347d100fd9e5bc532dfe41fe52
The Stig <TheStig294@users.noreply.github.com>
2022-12-12 14:46:13 +1030

    Added hypnotist trophy

---
+++ lua/ttt_trophies/trophies/hypnotist.lua
@@ -0,0 +1,32 @@
+local TROPHY = {}
+TROPHY.id = "hypnotist"
+TROPHY.title = "Risky move"
+TROPHY.desc = "As a Hypnotist, win the round after reviving a detective"
+TROPHY.rarity = 1
+
+function TROPHY:Trigger()
+    self.roleMessage = ROLE_HYPNOTIST
+    local revivedDetective = {}
+
+    self:AddHook("TTTPlayerRoleChangedByItem", function(ply, tgt, item)
+        if item:GetClass() == "weapon_hyp_brainwash" and TTTTrophies:IsGoodDetectiveLike(tgt) then
+            revivedDetective[ply] = true
+        end
+    end)
+
+    self:AddHook("TTTEndRound", function(result)
+        if result == WIN_TRAITOR then
+            for ply, _ in pairs(revivedDetective) do
+                self:Earn(ply)
+            end
+        end
+
+        table.Empty(revivedDetective)
+    end)
+end
+
+function TROPHY:Condition()
+    return ConVarExists("ttt_hypnotist_enabled") and GetConVar("ttt_hypnotist_enabled"):GetBool()
+end
+
+RegisterTTTTrophy(TROPHY)
