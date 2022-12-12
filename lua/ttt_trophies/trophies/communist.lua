5447c5c6b4bacc67c6a70b6587811cf6cdf765c1
The Stig <TheStig294@users.noreply.github.com>
2022-12-12 23:25:01 +1030

    Added communist trophy

---
+++ lua/ttt_trophies/trophies/communist.lua
@@ -0,0 +1,29 @@
+local TROPHY = {}
+TROPHY.id = "communist"
+TROPHY.title = "Our win, comrade"
+TROPHY.desc = "As a Communist, win with 5 or more players converted to communism"
+TROPHY.rarity = 3
+
+function TROPHY:Trigger()
+    self.roleMessage = ROLE_COMMUNIST
+
+    self:AddHook("TTTEndRound", function(result)
+        local communists = {}
+
+        for _, ply in ipairs(player.GetAll()) do
+            if ply:IsCommunist() and self:IsAlive(ply) then
+                table.insert(communists, ply)
+            end
+        end
+
+        if #communists >= 5 then
+            self:Earn(communists)
+        end
+    end)
+end
+
+function TROPHY:Condition()
+    return ConVarExists("ttt_communist_enabled") and GetConVar("ttt_communist_enabled"):GetBool()
+end
+
+RegisterTTTTrophy(TROPHY)
