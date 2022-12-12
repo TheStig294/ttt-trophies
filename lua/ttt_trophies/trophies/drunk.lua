8cfb7abf5f6c7c2231de54b9ffb280bfd038b946
The Stig <TheStig294@users.noreply.github.com>
2022-12-12 22:49:59 +1030

    Added drunk trophy

---
+++ lua/ttt_trophies/trophies/drunk.lua
@@ -0,0 +1,36 @@
+local TROPHY = {}
+TROPHY.id = "drunk"
+TROPHY.title = "Maximise the odds"
+TROPHY.desc = "As a Drunk, remember your role while only 1 other player is alive"
+TROPHY.rarity = 3
+
+function TROPHY:Trigger()
+    self.roleMessage = ROLE_DRUNK
+
+    self:AddHook("TTTWinCheckBlocks", function()
+        local drunk
+        local otherPlayer
+
+        for _, ply in ipairs(player.GetAll()) do
+            if self:IsAlive(ply) then
+                if ply:IsDrunk() then
+                    if IsPlayer(drunk) then return end
+                    drunk = ply
+                else
+                    if IsPlayer(otherPlayer) then return end
+                    otherPlayer = ply
+                end
+            end
+        end
+
+        if IsPlayer(drunk) and IsPlayer(otherPlayer) then
+            self:Earn(drunk)
+        end
+    end)
+end
+
+function TROPHY:Condition()
+    return ConVarExists("ttt_drunk_enabled") and GetConVar("ttt_drunk_enabled"):GetBool()
+end
+
+RegisterTTTTrophy(TROPHY)
