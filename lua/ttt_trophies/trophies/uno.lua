6954a9dc5a8fd341f5078d5db67194ef04f82f32
The Stig <TheStig294@users.noreply.github.com>
2022-12-13 19:31:55 +1030

    Added uno trophy

---
+++ lua/ttt_trophies/trophies/uno.lua
@@ -0,0 +1,22 @@
+local TROPHY = {}
+TROPHY.id = "uno"
+TROPHY.title = "no u"
+TROPHY.desc = "Kill someone with an uno reverse card"
+TROPHY.rarity = 3
+
+function TROPHY:Trigger()
+    self:AddHook("DoPlayerDeath", function(ply, attacker, dmg)
+        local inflictor = dmg:GetInflictor()
+        if not IsValid(inflictor) or not IsPlayer(attacker) then return end
+
+        if inflictor:GetClass() == "weapon_unoreverse" then
+            self:Earn(attacker)
+        end
+    end)
+end
+
+function TROPHY:Condition()
+    return weapons.Get("weapon_unoreverse") ~= nil
+end
+
+RegisterTTTTrophy(TROPHY)
