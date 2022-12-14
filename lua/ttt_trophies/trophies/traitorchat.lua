def37f99674d8fb8305c2fbfaac1748d6c5b5b87
The Stig <TheStig294@users.noreply.github.com>
2022-12-14 21:15:45 +1030

    Added traitor chat trophy

---
+++ lua/ttt_trophies/trophies/traitorchat.lua
@@ -0,0 +1,15 @@
+local TROPHY = {}
+TROPHY.id = "traitorchat"
+TROPHY.title = "Super secret traitor chat!"
+TROPHY.desc = "Communicate to your fellow traitors in secret using \"traitor chat\" (Press 'U')"
+TROPHY.rarity = 1
+
+function TROPHY:Trigger()
+    self:AddHook("PlayerSay", function(ply, txt, teamChat)
+        if teamChat and TTTTrophies:IsTraitorTeam(ply) then
+            self:Earn(ply)
+        end
+    end)
+end
+
+RegisterTTTTrophy(TROPHY)
