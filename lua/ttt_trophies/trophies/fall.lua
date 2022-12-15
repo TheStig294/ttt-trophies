cb53bf4b17128e6c49d74f463dfa6220d6e26a16
The Stig <TheStig294@users.noreply.github.com>
2022-12-15 14:45:56 +1030

    Added fall trophy

---
+++ lua/ttt_trophies/trophies/fall.lua
@@ -0,0 +1,33 @@
+local TROPHY = {}
+TROPHY.id = "fall"
+TROPHY.title = "Nice save!"
+TROPHY.desc = "Save yourself from a high damage fall by double-jumping"
+TROPHY.rarity = 2
+
+function TROPHY:Trigger()
+    local doubleJumping = {}
+
+    self:AddHook("OnPlayerHitGround", function(ply, inWater, onFloater, speed)
+        doubleJumping[ply] = false
+    end)
+
+    self:AddHook("PlayerButtonDown", function(ply, button)
+        if button == KEY_SPACE and not ply:OnGround() then
+            if button == KEY_SPACE and not ply:OnGround() and not doubleJumping[ply] then
+                local velocity = ply:GetVelocity().z
+
+                if velocity <= -700 then
+                    self:Earn(ply)
+                end
+            end
+
+            doubleJumping[ply] = true
+        end
+    end)
+end
+
+function TROPHY:Condition()
+    return ConVarExists("multijump_default_jumps") and GetConVar("multijump_default_jumps"):GetInt() == 1 and ConVarExists("multijump_can_jump_while_falling") and GetConVar("multijump_can_jump_while_falling"):GetBool()
+end
+
+RegisterTTTTrophy(TROPHY)
