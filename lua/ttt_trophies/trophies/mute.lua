14eb4dcd18f2db5469f3ff4061a485ef9c390071
The Stig <TheStig294@users.noreply.github.com>
2022-12-15 13:52:12 +1030

    Added mute trophy

---
+++ lua/ttt_trophies/trophies/mute.lua
@@ -0,0 +1,26 @@
+local TROPHY = {}
+TROPHY.id = "mute"
+TROPHY.title = "Don't wanna hear this noise"
+TROPHY.desc = "Use the mute button to mute and unmute music (Press 'M')"
+TROPHY.rarity = 1
+
+if CLIENT then
+    hook.Add("TTTMusicMuteButton", "TTTTrophiesMusicMuteButton", function()
+        net.Start("TTTTrophyMusicMuteButton")
+        net.SendToServer()
+    end)
+end
+
+function TROPHY:Trigger()
+    util.AddNetworkString("TTTTrophyMusicMuteButton")
+
+    net.Receive("TTTTrophyMusicMuteButton", function(len, ply)
+        self:Earn(ply)
+    end)
+end
+
+function TROPHY:Condition()
+    return ConVarExists("ttt_music_mute_m_button") and GetConVar("ttt_music_mute_m_button"):GetBool()
+end
+
+RegisterTTTTrophy(TROPHY)
