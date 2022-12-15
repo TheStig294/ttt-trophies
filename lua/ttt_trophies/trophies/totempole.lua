853cfa855d9b7a44071208c5307aa7d54d8af40a
The Stig <TheStig294@users.noreply.github.com>
2022-12-15 15:54:36 +1030

    Added totem pole trophy

---
+++ lua/ttt_trophies/trophies/totempole.lua
@@ -0,0 +1,19 @@
+local TROPHY = {}
+TROPHY.id = "totempole"
+TROPHY.title = "Totem pole"
+TROPHY.desc = "Survive to the end of a round while stood on another player or being stood on"
+TROPHY.rarity = 3
+
+function TROPHY:Trigger()
+    self.roleMessage = ROLE_INNOCENT
+
+    self:AddHook("TTTEndRound", function()
+        for _, ply in ipairs(player.GetAll()) do
+            if self:IsAlive(ply) and IsPlayer(ply:GetGroundEntity()) then
+                self:Earn({ply, ply:GetGroundEntity()})
+            end
+        end
+    end)
+end
+
+RegisterTTTTrophy(TROPHY)
