0da13ebe50886d14905e16cabc8d2b786b45e5b5
The Stig <TheStig294@users.noreply.github.com>
2022-12-15 15:44:52 +1030

    Added crouch trophy

---
+++ lua/ttt_trophies/trophies/crouch.lua
@@ -0,0 +1,33 @@
+local TROPHY = {}
+TROPHY.id = "crouch"
+TROPHY.title = "Squat walk"
+TROPHY.desc = "Stay crouched and alive from the beginning to the end of a round"
+TROPHY.rarity = 3
+
+function TROPHY:Trigger()
+    local startedCrouching = {}
+    local stoppedCrouching = {}
+
+    self:AddHook("PlayerButtonDown", function(ply, button)
+        if button == KEY_LCONTROL and GetRoundState() == ROUND_PREP then
+            startedCrouching[ply] = true
+            stoppedCrouching[ply] = false
+        end
+    end)
+
+    self:AddHook("PlayerButtonUp", function(ply, button)
+        if button == KEY_LCONTROL then
+            stoppedCrouching[ply] = true
+        end
+    end)
+
+    self:AddHook("TTTEndRound", function()
+        for _, ply in ipairs(player.GetAll()) do
+            if not stoppedCrouching[ply] and startedCrouching[ply] and self:IsAlive(ply) then
+                self:Earn(ply)
+            end
+        end
+    end)
+end
+
+RegisterTTTTrophy(TROPHY)
