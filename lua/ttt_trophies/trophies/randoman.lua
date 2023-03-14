1bab958dc86214dfd437e1cbf562e8117f685773
The Stig <TheStig294@users.noreply.github.com>
2023-03-14 19:52:12 +1030

    Fixed randoman trophy not being earned

--- lua/ttt_trophies/trophies/randoman.lua
+++ lua/ttt_trophies/trophies/randoman.lua
@@ -4,19 +4,23 @@ TROPHY.title = "What is happening?"
 TROPHY.desc = "As a Randoman, be alive while 4 or more randomats are active"
 TROPHY.rarity = 2
 
-function TROPHY:Trigger()
+function TROPHY:Trigger(randomanEarned)
     self.roleMessage = ROLE_RANDOMAN
 
-    self:AddHook("TTTRandomatTriggered", function(id, owner)
-        if #Randomat.ActiveEvents == 4 then
-            for _, ply in ipairs(player.GetAll()) do
-                if ply:IsRandoman() then
-                    self:Earn(ply)
-                end
+    if randomanEarned then
+        self:Earn(randomanEarned)
+    end
+end
+
+hook.Add("TTTRandomatTriggered", "TTTTrophiesRandoman", function(id, owner)
+    if #Randomat.ActiveEvents == 4 then
+        for _, ply in ipairs(player.GetAll()) do
+            if ply:IsRandoman() then
+                TROPHY:Trigger(ply)
             end
         end
-    end)
-end
+    end
+end)
 
 function TROPHY:Condition()
     return ConVarExists("ttt_randoman_enabled") and GetConVar("ttt_randoman_enabled"):GetBool()
