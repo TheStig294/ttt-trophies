f53baaa11311bae0d78dc5ae3c6f6c09ebb0db2b
The Stig <thestig294@gmail.com>
2023-10-25 23:25:56 +1030

    Added win condition to "All alone..." trophy

--- lua/ttt_trophies/trophies/veteran.lua
+++ lua/ttt_trophies/trophies/veteran.lua
@@ -1,29 +1,39 @@
 local TROPHY = {}
 TROPHY.id = "veteran"
 TROPHY.title = "All alone..."
-TROPHY.desc = "As a Veteran, be the last innocent alive"
+TROPHY.desc = "As a Veteran, be the last innocent alive and win"
 TROPHY.rarity = 3
 TROPHY.hidden = true
 
 function TROPHY:Trigger()
+    local veteranAlive
+
     self:AddHook("DoPlayerDeath", function()
         if GetRoundState() ~= ROUND_ACTIVE then return end
-        local veteranAlive
 
         for _, ply in ipairs(player.GetAll()) do
             if self:IsAlive(ply) then
                 if ply:GetRole() == ROLE_VETERAN then
-                    if veteranAlive then return end
+                    if veteranAlive then
+                        veteranAlive = nil
+
+                        return
+                    end
+
                     veteranAlive = ply
                 elseif TTTTrophies:IsInnocentTeam(ply) then
                     return
                 end
             end
         end
+    end)
 
-        if veteranAlive then
+    self:AddHook("TTTEndRound", function(result)
+        if IsValid(veteranAlive) and (result == WIN_INNOCENT or result == WIN_TIMELIMIT) and veteranAlive:GetRole() == ROLE_VETERAN then
             self:Earn(veteranAlive)
         end
+
+        veteranAlive = nil
     end)
 end
 
