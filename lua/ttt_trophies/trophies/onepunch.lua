e1c5e1fd07f0b7b6fcf78d13551533c9c40f03ce
thestig294 <thestig294@gmail.com>
2026-03-22 14:12:17 +1030

    Updated One Punch trophy for new classname for the One Punch weapon

--- lua/ttt_trophies/trophies/onepunch.lua
+++ lua/ttt_trophies/trophies/onepunch.lua
@@ -7,14 +7,14 @@ TROPHY.hidden = true
 
 function TROPHY:Trigger()
     self:AddHook("PlayerSwitchWeapon", function(ply, oldWep, newWep)
-        if TTTTrophies:IsGoodDetectiveLike(ply) and IsValid(newWep) and newWep:GetClass() == "one_punch_skin" then
+        if TTTTrophies:IsGoodDetectiveLike(ply) and IsValid(newWep) and newWep:GetClass() == "weapon_ttt_one_punch" then
             self:Earn(player.GetAll())
         end
     end)
 end
 
 function TROPHY:Condition()
-    return weapons.Get("one_punch_skin")
+    return weapons.Get("weapon_ttt_one_punch")
 end
 
 RegisterTTTTrophy(TROPHY)
